f05a47be33a0c1841f23f97ad1013256fbe2f6fb
Simon123-lab <63109996+Simon123-lab@users.noreply.github.com>
2021-10-17 13:15:28 +0500

    Update settings.py

--- growy/settings.py
+++ growy/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
