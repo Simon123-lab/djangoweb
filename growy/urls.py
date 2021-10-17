15adbb6f50219978f882a6b79f1304dfc57a4e93
Simon123-lab <63109996+Simon123-lab@users.noreply.github.com>
2021-10-17 12:17:57 +0500

    Update urls.py

--- growy/urls.py
+++ growy/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('service.urls')),
 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
+   
 
 ]
